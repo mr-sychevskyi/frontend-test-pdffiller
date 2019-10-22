d8c782bf5890621b87910e62a4cd6ac08bc5845a
Nick <n.sychevskyi@gmail.com>
2019-10-22 12:57:47 +0300

    Add icon highlighting for active payment card type

--- app/scripts/modules/cleave-config.js
+++ app/scripts/modules/cleave-config.js
@@ -2,9 +2,14 @@ const cleave = new Cleave('.card-input', {
   creditCard: true,
   onCreditCardTypeChanged: function (type) {
     if (type) {
-      const cardsList = document.querySelectorAll('.icons-list .svg');
+      const cardsList = [...document.querySelectorAll('.icons-list__item .svg')];
+      const isCardMatched = cardsList.some(icon => icon.classList.contains(`icon-${type}`));
 
-      [...cardsList].forEach(icon => {
+      isCardMatched
+        ? cardsList.forEach(icon => icon.classList.add('is-blur'))
+        : cardsList.forEach(icon => icon.classList.remove('is-blur'));
+
+      cardsList.forEach(icon => {
         icon.classList.contains(`icon-${type}`)
           ? icon.classList.add('is-active')
           : icon.classList.remove('is-active');
